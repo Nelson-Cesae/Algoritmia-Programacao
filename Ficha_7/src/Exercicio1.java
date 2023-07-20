51998ba8b1c4885b4bd42a0a4203337098444e02
Nelson <nelson.barbosa.prt_a@msft.cesae.pt>
2023-07-20 12:24:33 +0100

    Leitura_de_Ficheiros

---
+++ Ficha_7/src/Exercicio1.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class Exercicio1 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file = new File ("Files/exercicio_01.txt");
+
+        Scanner sc = new Scanner(file);
+
+
+
+    }
+}
