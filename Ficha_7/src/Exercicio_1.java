fd8ac33d865658221dd3eeb2c835bc99e3b1f049
Nelson <nelson.barbosa.prt_a@msft.cesae.pt>
2023-07-27 16:18:01 +0100

    Ficha7_Leitura_Ficheiros

--- Ficha_7/src/Exercicio_1.java
+++ Ficha_7/src/Exercicio_1.java
@@ -1,14 +1,13 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-public class Exercicio1 {
+public class Exercicio_1 {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        File file = new File ("Files/exercicio_01.txt");
+        File file = new File("Files/exercicio_01.txt");
 
         Scanner sc = new Scanner(file);
-
 
 
     }
