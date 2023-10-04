4c8c27bad5be4a521a962461e6fa1d11780f01a6
Nelson <nelson.barbosa.prt_a@msft.cesae.pt>
2023-10-04 16:25:45 +0100

    Ficha_Pratica_Avaliação

---
+++ TP_POO_Nelson_Barbosa/src/MeioAmbiente.java
@@ -0,0 +1,35 @@
+import Enums.FamiliaPlanta;
+
+import java.util.ArrayList;
+
+public class MeioAmbiente {
+
+    private String nome;
+    private double agua;
+    private ArrayList<SerVivo> seres;
+
+    /**
+     * Metodo construtor
+     * @param nome nome do meio ambiente
+     * @param agua litros de agua no meio ambiente
+     */
+    public MeioAmbiente(String nome, double agua) {
+        this.nome = nome;
+        this.agua = agua;
+        this.seres = new ArrayList<SerVivo>(seres);
+    }
+
+
+    public boolean plantaBebe (int indexPlanta){
+        if(this.) // usar um get na classe Planta e aqui ir buscar o get-"FamiliaPlanta"
+            // usar um remove para retirar a planta do meio ambiente caso retorne false
+    }
+
+    public boolean plantaComeInsetos(int indexPlanta){
+
+    }
+
+    public void plantaAbanaComVento(int indexPlanta){
+
+    }
+}
